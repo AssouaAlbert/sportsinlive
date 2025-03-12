4e071c234537a592d318313d5a63754d43dc5d44
Assoua Albert <assouaalbert@gmail.com>
2025-03-12 13:00:37 -0400

    Add .env files for all environment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,49 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react-swc";
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+
+  const isDev: boolean = mode.startsWith("dev");
+  const isProd: boolean = mode.startsWith("prod");
+
+  const apiURL = isDev ? "http://localhost:8081" : "http://localhost:3000"; 
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      open: true,
+      proxy: {
+        "/api": {
+          target: "http://localhost:8081",
+          changeOrigin: true,
+          rewrite: (path: string) => path.replace(/^\/api/, ""),
+        },
+      },
+    },
+    build: {
+      outDir: "build",
+      sourcemap: true,
+      minify: "terser",
+      chunkSizeWarningLimit: 1024,
+    },
+    resolve: {
+      alias: { "@": "/src" },
+    },
+    css: {
+      // css modules  camelCase
+      modules: { localsConvention: "camelCase", scopeBehaviour: "local" },
+      preprocessorOptions: {
+        scss: {
+          additionalData: '@import "@/styles/variables.scss";',
+          indentedSyntax: false,
+        },
+      },
+    },
+    define: {
+      "process.env.NODE_ENV": '"production"',
+      "process.env.REACT_APP_API_URL": '"http://localhost:3000"',
+    },
+  };
+});
